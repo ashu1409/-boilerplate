2b90353f10af9c8b86194eb3c0b63a8baccc32a6
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-05-16 14:03:55 +0530

    Create UserController.php

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+// src/Controller/UserController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="users", methods={"GET"})
+     */
+    public function index()
+    {
+        // Implement logic to display users (reserved for admins)
+    }
+}
