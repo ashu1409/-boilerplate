1689c30f19d97c8da6ad07c516f6cc1327768482
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-05-16 14:01:58 +0530

    Create AuthController.php

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,26 @@
+// src/Controller/AuthController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        // Handle user authentication
+    }
+
+    /**
+     * @Route("/logout", name="logout", methods={"POST"})
+     */
+    public function logout(Request $request)
+    {
+        // Handle user logout
+    }
+}
