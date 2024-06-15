2dc42049257e9addf2fe5e56bc9477c474a6fb38
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-06-15 16:23:12 +0530

    Create Payments.vue

---
+++ pages/Payments.vue
@@ -0,0 +1,38 @@
+<!-- pages/Payments.vue -->
+
+<template>
+  <div>
+    <h1>Your Payments</h1>
+    <ul>
+      <li v-for="payment in payments" :key="payment.id">
+        {{ payment.description }} - {{ payment.amount }}
+        <button @click="openModal(payment.id)">Locate</button>
+      </li>
+    </ul>
+    <location-modal v-if="showModal" :paymentId="selectedPaymentId" @close="closeModal" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      payments: [],
+      showModal: false,
+      selectedPaymentId: null,
+    };
+  },
+  methods: {
+    openModal(paymentId) {
+      this.selectedPaymentId = paymentId;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+  created() {
+    // Fetch user payments
+  },
+};
+</script>
