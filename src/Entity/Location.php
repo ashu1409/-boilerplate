3303ea466dee57b50c7926fccbc14e78ef340d50
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-06-15 16:44:12 +0530

    Create Location.php

---
+++ src/Entity/Location.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LocationRepository::class)
+ */
+class Location
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $longitude;
+
+    // getters and setters
+}
